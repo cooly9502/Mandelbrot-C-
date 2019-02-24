966e41c6eecf9cb2e21c43d54a4243d827b753d7
cooly9502 <techtag@hotmail.com>
2019-02-24 10:51:32 -0500

    Added Json reading, doesn't work yet.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <sstream>
 #include <math.h>
+#include <jsoncpp/json/json.h>
 
 //they're backwards, just deal with it im too lazy to fix it
 #define WIDTH 1080
@@ -21,25 +22,28 @@ using namespace std;
 int main(int argc, char* argv[]){
     int STARTFRAME   =   stoi(argv[1]); //string to int
     int ENDFRAME     =   stoi(argv[2]);
+    cout << STARTFRAME << "    " << ENDFRAME << endl;
     int VIDLENGTH    =   ENDFRAME - STARTFRAME;
     
     //for config file
     long double SCALE, xedit, yedit;
-    string line;
-    ifstream file ("config.txt");
-    if ( file.is_open() ) 
-    {
-        getline ( file, line ); SCALE      =     atof(line.c_str());
-        getline ( file, line ); xedit      =     atof(line.c_str());
-        getline ( file, line ); yedit      =     atof(line.c_str());
-        file.close();
-    }
+    ifstream ifs("config.json");
+    string::size_type sz;
+    Json::Reader reader;
+    Json::Value obj;
+    reader.parse(ifs, obj);
+    istringstream os( obj[ "scale" ] );
+    os >> SCALE
+    xedit = obj[ 'x' ];
+    yedit = obj[ 'y' ];
+
 
     //calculate starting scale
-    for ( int i = 0; i < ENDFRAME-STARTFRAME; i++){
+    for ( int i = 0; i < STARTFRAME; i++){
         SCALE -= SCALE * ZOOMSPEED;
+        cout << i << endl;
     }
-
+    cout << SCALE << endl;
     //constants 
     const double colorValRatio   =     ( MAXiter / 255) * 255;
     const double calcXvar        =     (WIDTH / 2 ) * SCALE;
@@ -50,6 +54,12 @@ int main(int argc, char* argv[]){
     //repeat over and over for each frame of video and output as number
     for (int f = STARTFRAME; f < ENDFRAME; f++)
     {
+        percentage_complete = (float(f) / float( ENDFRAME - STARTFRAME)) * 100 ;
+        cout << "   Render Progress: " << percentage_complete << "%" <<
+                "   Current Frame: " << f <<
+                "   Current Depth (scale): " << SCALE << 
+                "\r" << flush;
+
         FreeImage_Initialise();
         FIBITMAP * bitmap = FreeImage_Allocate(HEIGHT, WIDTH, BPP);
         RGBQUAD color;
@@ -95,16 +105,11 @@ int main(int argc, char* argv[]){
             }
         }
         //output IMAGE!!!
-        percentage_complete = (float(f) / float( ENDFRAME - STARTFRAME)) * 100 ;
-        cout << "   Render Progress: " << percentage_complete << "%" <<
-                "   Current Frame: " << f <<
-                "   Current Depth (scale): " << SCALE << 
-                "\r" << flush;
+        SCALE -= SCALE * ZOOMSPEED;
 
         fileOutputName = ("frames/frame_" + to_string(f) +".png").c_str();
         if (FreeImage_Save(FIF_PNG, bitmap, fileOutputName.c_str(), 0)){}
         FreeImage_DeInitialise();
-        SCALE -= SCALE * ZOOMSPEED;
     }
     return 0;
 }
